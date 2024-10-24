88b1af403ae7227924b02531b647b4c991d67513
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-10-24 22:57:13 +0600

    function added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,14 @@
-function play(){
+/* function play(){
     // hide the Home Screen,to hide the screen add the class 'hidden'
 
    const homeSection = document.getElementById('home-screen');
    homeSection.classList.add('hidden')
 
     // Show The PlayGround
-
+    const playGround = document.getElementById('play-ground');
+    playGround.classList.remove('hidden')
+} */
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground')
 }
