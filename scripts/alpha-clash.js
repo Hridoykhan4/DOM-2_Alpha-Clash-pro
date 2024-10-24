84e163eaf02a173e3dd5bc5be9d7d27cd917d0f7
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-10-24 22:42:06 +0600

    HomeScreen Hidden

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+function play(){
+    // hide the Home Screen,to hide the screen add the class 'hidden'
+
+   const homeSection = document.getElementById('home-screen');
+   homeSection.classList.add('hidden')
+
+    // Show The PlayGround
+
+}
